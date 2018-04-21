3fe107d2ca8b53473941eb6a4de70e3dd88bd3b9
takafumi toganoki <t.toganoki@smilekao.com>
2018-04-21 12:02:10 +0000

    Bootstrap correction

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,7 +5,7 @@
     <h1>メッセージ新規作成ページ</h1>
     
     <div class="row">
-        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-sm-offset2 col-md-offset-2 col-md-8 col-sm-offset2 col-md-offset-3 col-md-6 col-sm-offset3 ">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3 ">
     
      {!! Form::model($task, ['route' => 'tasks.store']) !!}
             
